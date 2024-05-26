e93f813a865425289b23b8d2dd336a9c911465e7
Muhammad Ikhsan Maulana Taqwim JTK D3 1B <117416421+Ikhsan310105@users.noreply.github.com>
2024-05-26 22:33:01 +0700

    fix model

---
+++ lib/model/user_prodi.dart
@@ -0,0 +1,25 @@
+class UserProdi {
+  int idProdi;
+  int idJurusan;
+  String namaProdi;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  UserProdi({
+    required this.idProdi,
+    required this.idJurusan,
+    required this.namaProdi,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory UserProdi.fromJson(Map<String, dynamic> json) {
+    return UserProdi(
+      idProdi: json['id_prodi'] ?? 0,
+      idJurusan: json['id_jurusan'] ?? 0,
+      namaProdi: json['nama_prodi'] ?? '',
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
+}
